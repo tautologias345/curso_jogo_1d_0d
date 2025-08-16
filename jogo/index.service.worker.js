fc9f39bef7df3389998e43001f3022893bf7d179
Eduardo de Quadros <eduardo.ciecompu.markedigi@gmail.com>
2025-08-15 22:21:39 -0300

    Atualizando as Guias Iniciais

--- jogo/index.service.worker.js
+++ jogo/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1755306946|957445989';
+const CACHE_VERSION = '1755307223|22717227';
 /** @type {string} */
 const CACHE_PREFIX = '1D_0D-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
